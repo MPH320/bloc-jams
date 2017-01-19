59bb2c446ab8cb36a47c77e2482e2b04672a8bf9
Michelangelo Pereira <michelangelo.ph@gmail.com>
2017-01-19 15:23:01 -0800

    Module 2 Checkpoint 9 -Dom Scripting:Events- Completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,18 +1,21 @@
- var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
  
-     var points = document.getElementsByClassName('point');
-     
-     var revealPoint = function() {
-         
-         for (var i = 0; i < points.length; i++)
-        {
-            points[i].style.opacity = 1;
-            points[i].style.transform = "scaleX(1) translateY(0)";
-            points[i].style.msTransform = "scaleX(1) translateY(0)";
-            points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-        }
-         
-     };
-     
-    revealPoint();    
+ var animatePoints = function(points) {
+    forEach(points);    
  };
+
+ window.onload = function() {
+     
+     if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+     
+     var sellingPoints = document.getElementsByClassName('selling-points')[0];
+     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+     window.addEventListener("scroll", function(event) {
+         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);   
+         }
+     });
+ }
